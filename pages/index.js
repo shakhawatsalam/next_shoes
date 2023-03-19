9abb44fafc3ced3674c13dd4b81cc17f2c707d3a
shakhawatsalam <shakhawatsalam726@gmail.com>
2023-03-19 15:50:26 +0600

    home page is almost done

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import HeroBanner from "@/components/HeroBanner";
 import Wrapper from "@/components/Wrapper"
-
+import ProductCard from "@/components/ProductCard";
 
 export default function Home() {
     return <main >
@@ -14,9 +14,20 @@ export default function Home() {
                 <div className="text-md md:text-xl">A lightweight Nike ZoomX midsole is combined with increased stack heights to help provide cushioning during extended stretches  of running</div>
             </div>
             {/* heading and paragraph end  */}
+            {/* Products Grid start  */}
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 my-14 px-5 md:px-0">
-                
+                <ProductCard />
+                <ProductCard />
+                <ProductCard />
+                <ProductCard />
+                <ProductCard />
+                <ProductCard />
+                <ProductCard />
+                <ProductCard />
+                <ProductCard />
+                <ProductCard />
             </div>
+            {/* Products Grid end */}
         </Wrapper>
     </main>;
 }
